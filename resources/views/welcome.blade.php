43baf18dcfa6d953a069c237560a43887156d188
2749345597@qq.com <dimister2017>
2017-03-06 17:32:28 +0800

    1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -31,7 +31,12 @@
             }
 
             .title {
-                font-size: 96px;
+                font-size: 66px;
+            }
+            .titleA{
+            	font-size: 32px;
+            	text-decoration: none;
+            	color: #000;
             }
         </style>
     </head>
@@ -39,7 +44,18 @@
         <div class="container">
             <div class="content">
                 <div class="title">Laravel 5.1.11 各模块演示实例 </div>
+                <a class="title titleA" href="#"> <<进入演示>> </a>
             </div>
         </div>
     </body>
+    <script type="text/javascript" src="http://apps.bdimg.com/libs/jquery/2.1.4/jquery.js"></script>
+    <script type="text/javascript">
+    	$(document).ready(function() {
+    		// console.log('RGB('+Math.ceil(Math .random()*255)+','+ceil(Math.random()*255)+','+ceil(Math.random()*255)+')');
+    		setInterval(function(){
+				$('.title').css('color','RGB('+Math.ceil(Math.random()*200)+','+Math.ceil(Math.random()*200)+','+Math.ceil(Math.random()*200)+')');
+    		},2000);
+    			
+    	});
+    </script>
 </html>
